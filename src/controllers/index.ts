49caf55880d32ebf6fc7c59cbb5ec20120ceec17
xaerose <alexandre.germonvillebellet@viacesi.fr>
2024-04-07 21:24:14 +0200

    add controllers actions

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,6 +3,7 @@ import { IIdentity } from 'cesieats-service-types/src/identity';
 import { Identity } from '../database';
 import jwt from 'jsonwebtoken';
 
+//Crée un compte et renvoie jwt
 const registerIdentity = async (req: Request, res: Response) => {
   try {
     const identity: IIdentity = {
@@ -23,24 +24,86 @@ const registerIdentity = async (req: Request, res: Response) => {
   }
 };
 
-const loginIdentity = (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello, world!' });
+//Connecte à un compte et renvoie jwt
+const loginIdentity = async (req: Request, res: Response) => {
+  try {
+    const identity: IIdentity = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const result = await Identity.findOne(identity);
+
+    if(!result) {
+      res.status(404).json({ message: 'email/password not found or incorrect' });
+    }
+
+    const token = jwt.sign(identity, process.env.JWT_KEY!);
+    res.status(200).json(token);
+
+  } catch (error) {
+    console.log('[IDENTITY-SERVICE] loginIdentity error: ', error);
+    res.status(400).json({ message: 'an unexpected error occurred' });
+  }
 };
 
-const deleteIdentity = (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello, world!' });
+//Supprime un compte
+const deleteIdentity = async (req: Request, res: Response) => {
+  try {
+    const result = await Identity.deleteOne({email: res.locals.identity.email});
+
+    if(!result) {
+      res.status(404).json({ message: 'email not found' });
+    }
+
+    res.status(200).json('Account succesfully deleted!');
+  } catch (error) {
+    console.log('[IDENTITY-SERVICE] deleteIdentity error: ', error);
+    res.status(400).json({ message: 'an unexpected error occurred' });
+  }
 };
 
-const editIdentity = (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello, world!' });
+//Edite un compte
+const editIdentity = async (req: Request, res: Response) => {
+  try {
+    await Identity.replaceOne({email: res.locals.identity.email}, {password: req.body.newPassword});
+    res.status(200).json('Account succesfully edited!');
+
+  } catch (error) {
+    console.log('[IDENTITY-SERVICE] editIdentity error: ', error);
+    res.status(400).json({ message: 'an unexpected error occurred' });
+  }
 };
 
-const createApiKeyIdentity = (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello, world!' });
+//Crée et associe une clé Appi au compte connecté
+const createApiKeyIdentity = async (req: Request, res: Response) => {
+  try {
+    var apiKey = crypto.randomUUID();
+    while(Identity.find({apiKey: apiKey}) != null) {
+      apiKey = crypto.randomUUID();
+    }
+    await Identity.replaceOne({email: res.locals.identity.email}, {apiKey: apiKey});
+    res.status(200).json(apiKey);
+
+  } catch (error) {
+    console.log('[IDENTITY-SERVICE] createApiKeyIdentity error: ', error);
+    res.status(400).json({ message: 'an unexpected error occurred' });
+  }
 };
 
-const verifyApiKeyIdentity = (req: Request, res: Response) => {
-  res.status(200).json({ message: 'Hello, world!' });
+//Vérifie la clé Api d'un compte
+const verifyApiKeyIdentity = async (req: Request, res: Response) => {
+  try {
+    if(Identity.find({apiKey: res.locals.identity.apiKey}) != null) {
+      res.status(200).json();
+    }
+    else {
+      res.status(401).json({ message: 'apiKey could not be verified' });
+    }
+
+  } catch (error) {
+    console.log('[IDENTITY-SERVICE] verifyApiKeyIdentity error: ', error);
+    res.status(400).json({ message: 'an unexpected error occurred' });
+  }
 };
 
 const controller = {
