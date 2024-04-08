833e57d4a9d9f18d4845d2381c7f7594f3b38046
Keanu <keanuharrell@hotmail.fr>
2024-04-09 00:19:18 +0200

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { connectRabbitMQ } from './messaging';
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 Promise.all([connectRabbitMQ(), connectMongoose()]).then(() => {
   app.get('/', (req: Request, res: Response) => {
