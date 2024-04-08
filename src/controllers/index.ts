1f91302d4de363ba5ea963ca5a16a15e010e8f42
Keanu <keanuharrell@hotmail.fr>
2024-04-08 12:34:40 +0200

    add middleware to routes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -33,13 +33,12 @@ const loginIdentity = async (req: Request, res: Response) => {
     };
     const result = await Identity.findOne(identity);
 
-    if(!result) {
+    if (!result) {
       res.status(404).json({ message: 'email/password not found or incorrect' });
     }
 
     const token = jwt.sign(identity, process.env.JWT_KEY!);
     res.status(200).json(token);
-
   } catch (error) {
     console.log('[IDENTITY-SERVICE] loginIdentity error: ', error);
     res.status(400).json({ message: 'an unexpected error occurred' });
@@ -49,9 +48,9 @@ const loginIdentity = async (req: Request, res: Response) => {
 //Supprime une identité
 const deleteIdentity = async (req: Request, res: Response) => {
   try {
-    const result = await Identity.deleteOne({email: res.locals.identity.email});
+    const result = await Identity.deleteOne({ email: res.locals.identity.email });
 
-    if(!result) {
+    if (!result) {
       res.status(404).json({ message: 'email not found' });
     }
 
@@ -65,9 +64,8 @@ const deleteIdentity = async (req: Request, res: Response) => {
 //Edite une identité
 const editIdentity = async (req: Request, res: Response) => {
   try {
-    await Identity.replaceOne({email: res.locals.identity.email}, {password: req.body.newPassword});
+    await Identity.replaceOne({ email: res.locals.identity.email }, { password: req.body.newPassword });
     res.status(200).json('Account succesfully edited!');
-
   } catch (error) {
     console.log('[IDENTITY-SERVICE] editIdentity error: ', error);
     res.status(400).json({ message: 'an unexpected error occurred' });
@@ -77,13 +75,12 @@ const editIdentity = async (req: Request, res: Response) => {
 //Crée et associe une clé Appi à une identité connectée
 const createApiKeyIdentity = async (req: Request, res: Response) => {
   try {
-    var apiKey = crypto.randomUUID();
-    while(Identity.find({apiKey: apiKey}) != null) {
+    let apiKey = crypto.randomUUID();
+    while (Identity.find({ apiKey: apiKey }) != null) {
       apiKey = crypto.randomUUID();
     }
-    await Identity.replaceOne({email: res.locals.identity.email}, {apiKey: apiKey});
+    await Identity.replaceOne({ email: res.locals.identity.email }, { apiKey: apiKey });
     res.status(200).json(apiKey);
-
   } catch (error) {
     console.log('[IDENTITY-SERVICE] createApiKeyIdentity error: ', error);
     res.status(400).json({ message: 'an unexpected error occurred' });
@@ -93,13 +90,11 @@ const createApiKeyIdentity = async (req: Request, res: Response) => {
 //Vérifie la clé Api d'une identité
 const verifyApiKeyIdentity = async (req: Request, res: Response) => {
   try {
-    if(Identity.find({apiKey: res.locals.identity.apiKey}) != null) {
+    if (Identity.find({ apiKey: res.locals.identity.apiKey }) != null) {
       res.status(200).json();
-    }
-    else {
+    } else {
       res.status(401).json({ message: 'apiKey could not be verified' });
     }
-
   } catch (error) {
     console.log('[IDENTITY-SERVICE] verifyApiKeyIdentity error: ', error);
     res.status(400).json({ message: 'an unexpected error occurred' });
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,16 +1,18 @@
 import express, { Router } from 'express';
 import controller from '../controllers';
+import { middleware } from '../middlewares';
 
 const router: Router = express.Router();
 
+// Public
 router.post('/register', controller.registerIdentity);
-
 router.post('/login', controller.loginIdentity);
 
+router.use(middleware);
+
+// Private
 router.put('/edit/{id}', controller.editIdentity);
-
 router.post('/createApiKey', controller.createApiKeyIdentity);
-
 router.post('/verifyApiKey', controller.verifyApiKeyIdentity);
 
 export default router;
